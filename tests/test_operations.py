d04afa0f681670a3bd48d5414d04537478f04c34
AbdulKhaliqAbdulAzeez <abdulkhailqabdulazeez@gmail.com>
2025-09-26 15:53:36 -0400

    style: changed the names of the add function and test

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,7 @@
 from src.operations import add, subtract, multiply, divide
 
-def test_addition():
+def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
