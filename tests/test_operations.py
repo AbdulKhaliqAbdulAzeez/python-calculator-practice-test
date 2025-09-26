f72742613fa42e691c5e4ea0db209a65a16d34f3
AbdulKhaliqAbdulAzeez <abdulkhailqabdulazeez@gmail.com>
2025-09-26 15:54:22 -0400

    test: created a test for subtract function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,3 +5,7 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
